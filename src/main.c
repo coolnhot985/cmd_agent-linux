3f51a2d8e2ea2f53c1407d781e4fca74aef90f17
mskang985 <mskang985@gamil.com>
2018-11-28 12:04:46 +0000

    init

---
+++ src/main.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+#include "json.h"
+#include "cmd_agent.h"
+
+#define BUF_LEN 128
+
+int main(int argc, char *argv[]) {
+    char    buf[BUF_LEN+1]  = {0, };
+    char    *buff_send      = NULL;
+    char    *haddr          = "13.125.12.123";
+    int     fd              = 0;
+    int     n               = 0;
+    struct sockaddr_in      server_addr;
+
+    if((fd = socket(PF_INET, SOCK_STREAM, 0)) < 0) {
+        printf("can't create socket\n");
+        return 0;
+    }
+
+    bzero((char *)&server_addr, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = inet_addr(haddr);
+    server_addr.sin_port = htons(88);
+
+    if (connect(fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+        printf("can't connect.\n");
+        return 0;
+    }
+    
+    buff_send = tcp_hello();
+
+    while((n = read(fd, buf, BUF_LEN)) > 0) {
+        buf[n] = '\0';
+        printf("%s", buf);
+    }
+    
+    close(fd);
+}
+
+
